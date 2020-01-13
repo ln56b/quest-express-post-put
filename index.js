1874f787314167dcb13e537938adf178450b74bc
Hélène Baudrand <baudrand.helene@gmail.com>
2020-01-13 21:31:42 +0100

    added middlewares in put user

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ app.post('/api/users', [
   check('email').isEmail(),
   // Check that password length is ok
   check('password').isLength({ min: 8 }),
+  // Check that name length is ok
+  check('name').isLength({ min: 3 }),
 ],
 (req, res) => {
   const errors = validationResult(req);
@@ -48,9 +50,81 @@ app.post('/api/users', [
         sql: err.sql,
       });
     }
-    res.json(results);
+    return connection.query('SELECT * FROM user WHERE id = ?', results.insertId, (err2, records) => {
+      if (err2) {
+        return res.status(500).json({
+          error: err2.message,
+          sql: err2.sql,
+        });
+      }
+      // If no error, records is an array from which we use the first item
+      const insertedUser = records[0];
+      // Extract all the fields except from password as a new object(user)
+      const { password, ...user } = insertedUser;
+      // Get the host + port (localhost: 3000) from the request headers
+      const host = req.get('host');
+      // Compute the full location, e.g. http://localhost:3000/api/users/122
+      // This will help the client to know where the new resource can be found
+      const location = `http://${host}${req.url}/${user.id}`;
+      return res
+        .status(201)
+        .set('Location', location)
+        .json(user);
+    });
   });
 });
+
+// Define userValidationMiddleware
+const userValidationMiddleWare = [
+  // Check that email is valid
+  check('email').isEmail(),
+  // Check that password length is ok
+  check('password').isLength({ min: 8 }),
+  // Check that name length is ok
+  check('name').isLength({ min: 3 }),
+];
+
+// PUT user with express-validator
+app.put('/api/users/:id',
+  userValidationMiddleWare,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    // If no error, update the user corresponding to id
+    const idUser = req.params.id;
+    const formData = req.body;
+    connection.query('UPDATE user SET ? WHERE id = ?', [formData, idUser], (err, results) => {
+      if (err) {
+        res.status(500).json({
+          error: err.message,
+          sql: err.sql,
+        });
+      }
+      return connection.query('SELECT * FROM user WHERE id = ?', idUser, (err2, records) => {
+        if (err2) {
+          return res.status(500).json({
+            error: err2.message,
+            sql: err2.sql,
+          });
+        }
+        // If no error, records is an array from which we use the first item
+        const modifiedUser = records[0];
+        // Extract all the fields except from password as a new object(user)
+        const { password, ...user } = modifiedUser;
+        // Get the host + port (localhost: 3000) from the request headers
+        const host = req.get('host');
+        // Compute the full location, e.g. http://localhost:3000/api/users/122
+        // This will help the client to know where the new resource can be found
+        const location = `http://${host}${req.url}/${user.id}`;
+        return res
+          .status(200)
+          .set('Location', location)
+          .json(user);
+      });
+    });
+  });
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
